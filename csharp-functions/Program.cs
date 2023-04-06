5f9d2f91fbd917a2761a14d9f8c04299f80d23d8
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-06 16:18:27 +0200

    quarta funzione

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -17,9 +17,39 @@
             //Console.WriteLine($"Il quadrato di {num} è {Quadrato(num)} ");
 
             //3 funzione
-            int[] arrayNum = { 4, 2, 8, 12, 6, 17, 22, 9 };
+            //int[] arrayNum = { 4, 2, 8, 12, 6, 17, 22, 9 };
+            //int[] arrayCopia = (int[])arrayNum.Clone();
+            //ElevaArrayAlQuadrato(arrayCopia);
+            //Console.Write("[");
+            //for (int i = 0; i < arrayCopia.Length; i++)
+            //{
+
+            //    Console.Write(arrayCopia[i]);
+
+            //    if (arrayCopia[i] != arrayCopia[arrayCopia.Length - 1])
+            //        Console.Write(",");
+
+            //}
+            //Console.Write("]");
+
+            //4 funzione
+
+            int[] arrayNum = { 2, 6, 7, 5, 3, 9};
+            Console.Write("[");
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+
+                Console.Write(arrayNum[i]);
+
+                if (arrayNum[i] != arrayNum[arrayNum.Length - 1])
+                    Console.Write(",");
+
+            }
+            Console.WriteLine("]");
+
+       
             int[] arrayCopia = (int[])arrayNum.Clone();
-            ElevaArrayAlQuadrato(arrayCopia);
+            int[] arrayQuadrato =  ElevaArrayAlQuadrato(arrayCopia);
             Console.Write("[");
             for (int i = 0; i < arrayCopia.Length; i++)
             {
@@ -30,35 +60,49 @@
                     Console.Write(",");
 
             }
-            Console.Write("]");
+            Console.WriteLine("]");
+            Console.Write("[");
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+
+                Console.Write(arrayNum[i]);
+
+                if (arrayNum[i] != arrayNum[arrayNum.Length - 1])
+                    Console.Write(",");
+
+            }
+            Console.WriteLine("]");
+            Console.WriteLine($"La somma dei numeri è {sommaElementiArray(arrayNum)}");
+            Console.WriteLine($"La somma dei numeri al quadrato è {sommaElementiArray(arrayQuadrato)}");
+
 
 
         }
         //1 funzione
 
-        //public static void StampaArray(int[] array)
-        //{
-        //    Console.Write("[");
-        //    for (int i = 0; i < array.Length; i++)
-        //    {
+        public static void StampaArray(int[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
 
-        //        Console.Write(array[i]);
+                Console.Write(array[i]);
 
-        //        if (array[i] != array[array.Length-1])
-        //         Console.Write(",");
+                if (array[i] != array[array.Length - 1])
+                    Console.Write(",");
 
-        //    }
-        //    Console.Write("]");
-        //}
+            }
+            Console.Write("]");
+        }
 
 
         //2 funzione
-        //public static int Quadrato(int num)
-        //{
-        //    int quadrato = num*num;
-        //    return quadrato;
+        public static int Quadrato(int num)
+        {
+            int quadrato = num * num;
+            return quadrato;
 
-        //}
+        }
 
 
         //3 funzione
@@ -72,6 +116,20 @@
             return array;
 
         }
+
+        //4 funzione
+
+        public static int sommaElementiArray(int[] array)
+        {
+            int somma = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                somma = somma + array[i];
+            }
+            return somma;
+
+        }
+
 
     }
 }
