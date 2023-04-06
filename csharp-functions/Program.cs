fc8511c2a8ffe14149216c35a9f2d42aba8b37c5
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-06 15:23:05 +0200

    Add project files.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace csharp_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static void StampaArray(int[] array)
+        {
+
+        }
+    }
+}
